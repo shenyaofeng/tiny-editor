ce3d2de0765ca80f1d86250835a86421f283ee47
shenyaofeng <1051406614@qq.com>
2025-08-01 11:44:26 +0800

    fix(mind):完善国际化

--- packages/fluent-editor/src/modules/mind/modules/control-panel.ts
+++ packages/fluent-editor/src/modules/mind/modules/control-panel.ts
@@ -1,8 +1,76 @@
+import type FluentEditor from '../../../core/fluent-editor'
 import type MindmapPlaceholderBlot from '../formats/mind-blot'
+import { CHANGE_LANGUAGE_EVENT } from '../../../config'
+import { I18N } from '../../../modules/i18n'
+
+class ControlPanelHandler {
+  private texts: Record<string, string>
+  private lang: string
+  getText(key: keyof Record<string, string>): string {
+    return this.texts[key]
+  }
+
+  constructor(private quill: FluentEditor, private blot: MindmapPlaceholderBlot) {
+    this.lang = 'en-US'
+    this.texts = this.resolveTexts()
+    this.quill.emitter.on(CHANGE_LANGUAGE_EVENT, (lang: string) => {
+      this.lang = lang
+      this.texts = this.resolveTexts()
+      this.updateControlPanelTexts()
+    })
+  }
+
+  resolveTexts() {
+    return {
+      zoomOut: I18N.parserText('mindmap.controlPanel.zoomOut', this.lang),
+      zoomIn: I18N.parserText('mindmap.controlPanel.zoomIn', this.lang),
+      fit: I18N.parserText('mindmap.controlPanel.fit', this.lang),
+      back: I18N.parserText('mindmap.controlPanel.back', this.lang),
+      forward: I18N.parserText('mindmap.controlPanel.forward', this.lang),
+      export: I18N.parserText('mindmap.controlPanel.export', this.lang),
+      import: I18N.parserText('mindmap.controlPanel.import', this.lang),
+      inserChildNode: I18N.parserText('mindmap.controlPanel.inserChildNode', this.lang),
+      inserNode: I18N.parserText('mindmap.controlPanel.inserNode', this.lang),
+      inserParentNode: I18N.parserText('mindmap.controlPanel.inserParentNode', this.lang),
+      removeNode: I18N.parserText('mindmap.controlPanel.removeNode', this.lang),
+
+      zoomOutTitle: I18N.parserText('mindmap.controlPanel.zoomOutTitle', this.lang),
+      zoomInTitle: I18N.parserText('mindmap.controlPanel.zoomInTitle', this.lang),
+      fitTitle: I18N.parserText('mindmap.controlPanel.fitTitle', this.lang),
+      backTitle: I18N.parserText('mindmap.controlPanel.backTitle', this.lang),
+      forwardTitle: I18N.parserText('mindmap.controlPanel.forwardTitle', this.lang),
+      exportTitle: I18N.parserText('mindmap.controlPanel.exportTitle', this.lang),
+      importTitle: I18N.parserText('mindmap.controlPanel.importTitle', this.lang),
+      inserChildNodeTitle: I18N.parserText('mindmap.controlPanel.inserChildNodeTitle', this.lang),
+      inserNodeTitle: I18N.parserText('mindmap.controlPanel.inserNodeTitle', this.lang),
+      inserParentNodeTitle: I18N.parserText('mindmap.controlPanel.inserParentNodeTitle', this.lang),
+      removeNodeTitle: I18N.parserText('mindmap.controlPanel.removeNodeTitle', this.lang),
+    }
+  }
+
+  updateControlPanelTexts() {
+    const controlItems = this.blot.domNode.querySelectorAll('.mindmap-control-item')
+    controlItems.forEach((item) => {
+      const icon = item.querySelector('i')
+      if (icon) {
+        const iconClass = icon.className.split('-')[2]
+        if (this.texts[iconClass]) {
+          const textSpan = item.querySelector('.mindmap-control-text')
+          if (textSpan) {
+            textSpan.textContent = this.texts[iconClass]
+          }
+          (item as HTMLElement).title = this.texts[`${iconClass}Title`] || ''
+        }
+      }
+    })
+  }
+}
+
+const controlPanelHandlers = new WeakMap<MindmapPlaceholderBlot, ControlPanelHandler>()
 
 const DISABLED_OPACITY = '0.5'
 const ENABLED_OPACITY = '1'
-export function createControlPanel(blot: MindmapPlaceholderBlot): void {
+export function createControlPanel(blot: MindmapPlaceholderBlot, quill: FluentEditor): void {
   let isStart = true
   let isEnd = true
   // 右上的控制面板
@@ -14,25 +82,29 @@ export function createControlPanel(blot: MindmapPlaceholderBlot): void {
   // 左上的控制面板
   const controlLeftUpPanel = document.createElement('div')
   controlLeftUpPanel.className = 'mindmap-left-up-control'
-  const zoomOutBtn = createControlItem('zoomOut', '缩小', '缩小思维导图', () => handleZoomOut(blot))
-  const zoomInBtn = createControlItem('zoomIn', '放大', '放大思维导图', () => handleZoomIn(blot))
-  const resetBtn = createControlItem('fit', '适应', '恢复逻辑原有尺寸', () => handleResetZoom(blot))
-  const backBtn = createControlItem('back', '上一步', '回退到上一步', () => {
+
+  const handler = new ControlPanelHandler(quill, blot)
+  controlPanelHandlers.set(blot, handler)
+
+  const zoomOutBtn = createControlItem('zoomOut', handler.getText('zoomOut'), handler.getText('zoomOutTitle'), () => handleZoomOut(blot))
+  const zoomInBtn = createControlItem('zoomIn', handler.getText('zoomIn'), handler.getText('zoomInTitle'), () => handleZoomIn(blot))
+  const resetBtn = createControlItem('fit', handler.getText('fit'), handler.getText('fitTitle'), () => handleResetZoom(blot))
+  const backBtn = createControlItem('back', handler.getText('back'), handler.getText('backTitle'), () => {
     if (!isStart) {
       blot.mm.execCommand('BACK')
     }
   })
-  const forwardBtn = createControlItem('forward', '下一步', '前进到下一步', () => {
+  const forwardBtn = createControlItem('forward', handler.getText('forward'), handler.getText('forwardTitle'), () => {
     if (!isEnd) {
       blot.mm.execCommand('FORWARD')
     }
   })
-  const exportJSON = createControlItem('export', '导出', '导出JSON文件', () => handleExport(blot))
-  const importJSON = createControlItem('import', '导入', '导入JSON文件', () => handleImport(blot))
-  const insertChildNode = createControlItem('inserChildNode', '子节点', '插入子节点', () => handleInsertChildNode(blot))
-  const insertNode = createControlItem('inserNode', '同级节点', '插入同级节点', () => handleInsertNode(blot))
-  const insertParentNode = createControlItem('inserParentNode', '父节点', '插入父节点', () => handleInsertParentNode(blot))
-  const removeNode = createControlItem('removeNode', '删除', '删除节点', () => handleRemoveNode(blot))
+  const exportJSON = createControlItem('export', handler.getText('export'), handler.getText('exportTitle'), () => handleExport(blot))
+  const importJSON = createControlItem('import', handler.getText('import'), handler.getText('importTitle'), () => handleImport(blot))
+  const insertChildNode = createControlItem('inserChildNode', handler.getText('inserChildNode'), handler.getText('inserChildNodeTitle'), () => handleInsertChildNode(blot))
+  const insertNode = createControlItem('inserNode', handler.getText('inserNode'), handler.getText('inserNodeTitle'), () => handleInsertNode(blot))
+  const insertParentNode = createControlItem('inserParentNode', handler.getText('inserParentNode'), handler.getText('inserParentNodeTitle'), () => handleInsertParentNode(blot))
+  const removeNode = createControlItem('removeNode', handler.getText('removeNode'), handler.getText('removeNodeTitle'), () => handleRemoveNode(blot))
 
   const updateButtonState = (index: number, len: number) => {
     isStart = index <= 0
