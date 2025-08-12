1f3c0af088ff5be21730616cba16973c30602b77
shenyaofeng <1051406614@qq.com>
2025-08-12 18:45:00 +0800

    fix(flow-chart):修正中文的国际化

--- packages/fluent-editor/src/modules/flow-chart/i18n/zh-cn.ts
+++ packages/fluent-editor/src/modules/flow-chart/i18n/zh-cn.ts
@@ -1,8 +1,8 @@
 export const FLOW_CHART_ZH_CN = {
   'flowChart.controlPanel.export': '导出',
   'flowChart.controlPanel.import': '导入',
-  'flowChart.controlPanel.exportTitle': '导出思维导图',
-  'flowChart.controlPanel.importTitle': '导入思维导图',
+  'flowChart.controlPanel.exportTitle': '导出流程图',
+  'flowChart.controlPanel.importTitle': '导入流程图',
   'flowChart.dndPanel.selection': '选区',
   'flowChart.dndPanel.rectangle': '矩形',
   'flowChart.dndPanel.circle': '圆形',
